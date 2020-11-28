6325c908f3e2de9afc43a6c87c589faeb1555fac
Kenneth Shen <juntaosh@usc.edu>
2020-11-28 12:57:18 -0800

    Adds Video classification and trained model; Also adds front-end working flask code.

--- Front-end/app.py
+++ Front-end/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# endpoint for index.html
+@app.route('/', methods=["GET", "POST"])
+def homePage():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        videoes = [[0, "flower.rgb", "90%"],
+                   [1, "cars.rgb", "50%"],
+                   [2, "movie.rgb", "10%"],
+                   [3, "interview.rgb", "10%"]
+                   ]
+        query = request.form.get("query")
+        return render_template("search_results.html",
+                               given_query=query,
+                               matched_videos=videoes)
 
 
 if __name__ == '__main__':
