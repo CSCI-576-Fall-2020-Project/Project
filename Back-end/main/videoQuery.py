a8d41c92d6421cec7ff406a1f53e3d29e9eecc15
YaqiS <52696519+YaqiS@users.noreply.github.com>
2020-12-04 08:27:50 -0800

    Add files via upload

--- Back-end/main/videoQuery.py
+++ Back-end/main/videoQuery.py
@@ -1,9 +1,33 @@
 import os
 from glob import glob
 import pickle
-from main import getLabel
+# from main import getLabel
 from Compare import compareTwoVideos
+from videoclassifier import runVideoClassificationOn
 import sys
+def getLabel(isQuery, videoName):
+    
+    #keyframe less than 5 frames classification: sports, interview, concerts
+    # LessKeyFramesClass = set(["sport","interview","concerts"])
+    
+    if isQuery:
+        labels = runVideoClassificationOn("./static/QueryVideos/"+videoName+".mp4")
+        # length = len(labels)
+        
+        labelList = []
+        for label in labels:
+            labelList.append(label[0])
+            
+        # Hardcode Exception
+        if set(labelList) == set(["ads", "cartoon"]):
+            labelList = ["ads"]
+        if set(labelList) == set(["ads", "movies"]):
+            labelList = ["movies"]
+        if set(labelList) == set(["movies", "interview"]):
+            labelList = ["interview"]
+        return labelList
+    else:
+        return ["interview"]
 def videoQuery(argv):
     path = argv[0].split("/")
     name = path[-1]
